caf7a750ab4cab410a4c790eb217faebdd3d91fa
dave <davidpollak4@gmail.com>
2013-08-14 15:22:08 -0400

    bug fix local host wrong end

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Omrails::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   #In production host should be set to domain name
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }d
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' } 
 end
